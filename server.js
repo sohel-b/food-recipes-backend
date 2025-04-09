e2537f5fef49c2069475d1544361c39e034f7f36
sohelbepari <bepari.sohel@unifo.in>
2025-04-09 15:23:05 +0530

    addedPort

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import cors from "cors"
 import { getAllRecipes, getRecipeByName, getRecipesByIngredients } from "./routes/index.js";
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
